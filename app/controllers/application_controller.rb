47401b676eb0354a8c430d1949a5967afa655034
tvbach <tvbach20@gmail.com>
2022-07-16 15:51:17 +0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_user!
+
+  attr_reader :current_user
+
+  def authorize_user!
+    pattern = /^Bearer /
+    header = request.authorization
+
+    @current_user = DecodeToken.new(header.split()[1]).call
+
+    if header&.match(pattern)
+      return if @current_user
+
+      render status: 401, json: { messages: 'Token' } 
+    else
+      render status: 401, json: { messages: 'No token' }
+    end
+  end
 end
